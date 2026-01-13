747b734e955840603c32052488053e5aa9c3837a
Agustin <abarrionuevo205@gmail.com>
2026-01-13 19:57:42 -0300

    Agregado .gitignore para ignorar archivos temporales

--- obj/Debug/net8.0/FarmaciaSantaRita.AssemblyInfo.cs
+++ obj/Debug/net8.0/FarmaciaSantaRita.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FarmaciaSantaRita")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c786853dfd6104885b025f32a292bb0183a03b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("FarmaciaSantaRita")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FarmaciaSantaRita")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

f431c7f37207c63663cdea835bef605d1839e841
Agustin <abarrionuevo205@gmail.com>
2026-03-15 12:36:58 -0300

    Agrega propiedad Eliminado a Boletum

--- Models/Boletum.cs
+++ Models/Boletum.cs
@@ -19,6 +19,8 @@ public partial class Boletum
 
     public string Categoria { get; set; } = null!;
 
+    public bool Eliminado { get; set; } = false;
+
     public string Detalle { get; set; } = null!;
 
     public virtual Proveedor IdproveedorNavigation { get; set; } = null!;
